f63396add9fac73ba3a1137b667a4dd3b3b1d4c4
jte_fengdou <jte_fengdou@jte.com>
2021-03-01 14:32:14 +0800

    component

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {HeaderModule} from './modules/header/header.module'
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HeaderModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
